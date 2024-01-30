7c029cd03c420026ecdbe6914592fa3073e24a20
Daniel <danlixuan@gmail.com>
2024-01-29 20:37:12 -0600

    line search update to altnerative solver

--- src/pycvxcluster/algos/ssnal_alt.py
+++ src/pycvxcluster/algos/ssnal_alt.py
@@ -8,15 +8,12 @@ import scipy
 import scipy.sparse
 import scipy.sparse.linalg as sla
 
+
+import math
+
 # from numba import jit, njit, prange
 
 # Preliminaries
-
-
-# @jit(nopython=True, parallel=True, fastmath=True)
-# def sum_with_numba(X, hat_r, hat_c, D, alpha_less_1):
-#     return np.sum((X[:, hat_r] - X[:, hat_c]) * D[:, alpha_less_1], axis=0)
-
 
 class B(pylops.LinearOperator):
     r"""
@@ -30,11 +27,11 @@ class B(pylops.LinearOperator):
 
     def _matmat(self, X):
         # return right_multiply(X, self.map.data, self.map.indices, self.map.indptr, self.map.shape)
-        return X @ self.map
+        return (self.map.T @ X.T).T
 
     def _adjoint(self, Z):
         # return right_multiply_transpose(Z, self.map.data, self.map.indices, self.map.indptr, self.map.shape)
-        return Z @ self.map.T
+        return (self.map @ Z.T).T
 
 
 def fnorm(X):
@@ -48,7 +45,7 @@ def fnormsq(X):
 
 
 def matinner(X, Y):
-    return np.trace(np.inner(X, Y))
+    return np.einsum('ij,ij->', X, Y)
 
 
 def column_norms(X):
@@ -56,7 +53,8 @@ def column_norms(X):
 
 
 class SSNAL:
-    r""" """
+    r""" 
+    """
 
     def __init__(self, A, weights_matrix, gamma):
         # Initialization
@@ -64,7 +62,9 @@ class SSNAL:
         self.d, N = A.shape
         self.n = weights_matrix.shape[0]
         self.nz_r, self.nz_c = np.nonzero(np.triu(weights_matrix))
-        self.weights = gamma * weights_matrix[self.nz_r, self.nz_c]
+        self.pre_weights = weights_matrix[self.nz_r, self.nz_c]
+        self.gamma = gamma
+        #self.weights = gamma * weights_matrix[self.nz_r, self.nz_c]
         self.E = len(self.nz_r)
         # self.J = np.zeros((self.n,self.n))
         # self.J[self.nz_r, self.nz_c] = 1
@@ -76,6 +76,17 @@ class SSNAL:
         cJ[self.nz_c, np.arange(self.E)] = -1
         cJ = cJ.tocsr()
         self.Bop = B(cJ)
+    
+    @property
+    def gamma(self):
+        return self._gamma
+
+    @gamma.setter
+    def gamma(self, value):
+        if value < 0:
+            raise ValueError("gamma must be non-negative")
+        self.weights = value * self.pre_weights
+        self._gamma = value
 
     def pU(self, U):
         return np.dot(self.weights, column_norms(U))
@@ -148,13 +159,15 @@ class SSNAL:
         )
 
     @staticmethod
-    def sigma_update(iter):
+    def sigma_update(itera):
         sigma_update_iter = 2
-        if iter < 20:
+        if itera < 10:
+            sigma_update_iter = 2
+        elif itera < 20:
             sigma_update_iter = 3
-        elif iter < 200:
-            sigma_update_iter = 3
-        elif iter < 500:
+        elif itera < 500:
+            sigma_update_iter = 10
+        else:
             sigma_update_iter = 10
         return sigma_update_iter
 
@@ -175,27 +188,58 @@ class SSNAL:
         sigmai = sigma0
 
         termination = 1
+        prim_win = 0
+        dual_win = 0
+        ncg_iter = 50
+        ncg_tol = 1e-6
 
-        for iter in range(max_iter):
+        for itera in range(max_iter):
             # Step 1
-            Xi = self.ssncg(Xi, Ui, Zi, sigmai)
+
+            #print(ncg_tol)
+            
+            Xi = self.ssncg(Xi, Ui, Zi, sigmai, max_iter=ncg_iter, tolerance=ncg_tol)
             Ui = self.prox_pU(self.Bop._matmat(Xi) + Zi / sigmai, 1 / sigmai)
             # Step 2
 
             Zi = Zi + sigmai * self.kkt_3(Xi, Ui)
             # Check stopping criterion
             kkt_residual = self.kkt_residual(Xi, Ui, Zi)
-            print("kkt_residual: ", kkt_residual)
+            #print("kkt_residual: ", kkt_residual)
             prim = self.primal(Xi, Ui)
             dual = self.dual(Xi, Ui, Zi)
             prim_dual_gap = np.abs(prim - dual) / (1 + np.abs(prim) + np.abs(dual))
-            print("prim_dual_gap: ", prim_dual_gap)
+            #print("prim_dual_gap: ", prim_dual_gap)
+            if prim_dual_gap < eps:
+                termination = 0
+                break
             if np.max(kkt_residual) < eps:
                 termination = 0
                 break
             # Step 3
-            sigmai = self.sigma_update(iter)
-
+            if kkt_residual[0] < kkt_residual[1]:
+                prim_win += 1
+            else:
+                dual_win += 1
+            sigma_ui = self.sigma_update(itera)
+            sigmascale = 5
+            sigmamax = 135
+            if itera % sigma_ui == 0:
+                if itera % sigma_ui == 0:
+                    sigmamin = 1e-4
+                    if prim_win > max(1, 1.2 * dual_win):
+                        prim_win = 0
+                        sigmai = max(sigmamin, sigmai / sigmascale)
+                    elif dual_win > max(1, 1.2 * prim_win):
+                        dual_win = 0
+                        sigmai = min(sigmamax, sigmai * sigmascale)
+            if kkt_residual[0] < 1e-5:
+                ncg_iter = max(ncg_iter, 30)
+            elif kkt_residual[0] < 1e-3:
+                ncg_iter = max(ncg_iter, 30)
+            elif kkt_residual[0] < 1e-1:
+                ncg_iter = max(ncg_iter, 20)
+            print(itera)
         return Xi, Ui, Zi, termination
 
     # SSNCG
@@ -205,22 +249,48 @@ class SSNAL:
         U,
         Z,
         sigma,
-        mu=0.5,
-        tau=0.5,
-        eta=1,
-        delta=0.9,
-        max_iter=40,
+        mu=1e-4,
+        tau=1,
+        eta=1e-2,
+        delta=0.95,
+        max_iter=20,
         tolerance=1e-6,
         cg_max_iter=300,
     ):
         # Initialization
         Xj = X0
         sigmainv = 1 / sigma
-        mj = 0
-        mjs = np.zeros(max_iter)
-        score = 0
-        for iter in range(max_iter):
-            # print('ssncg iter: ', iter)
+
+        normborg = 1 + fnorm(self.A)
+        Rd = np.maximum(column_norms(Z) - self.weights, 0)
+        normRd = np.sum(Rd)
+        normU = fnorm(U)
+        Rp = self.kkt_3(X0, U)
+        normRp = fnorm(Rp)
+
+        #max_iter = 100
+        for itera in range(max_iter):
+            #print('ssncg iter: ', iter)
+
+            grad_phi_Xj = self.grad_phi(Xj, Z, sigma)
+            norm_grad_phi_Xj = fnorm(grad_phi_Xj)
+
+
+            priminf_sub = norm_grad_phi_Xj
+            dualinf_sub = normRd / (1 + normborg)
+            if max(priminf_sub, dualinf_sub) < tolerance:
+                tolsubconst = 0.9
+            else:
+                tolsubconst = 0.5
+            tolsub = np.maximum(np.minimum(1, 0.5 * normRp / (1 + normU)), tolsubconst * tolerance)
+            #print(np.nanmax(norm_grad_phi_Xj), np.nanmax(tolsub))
+            if norm_grad_phi_Xj < tolsub and itera > 0:
+                print('subproblem converged')
+                break
+            elif max(priminf_sub, dualinf_sub) < 0.5 * tolerance:
+                print('here2')
+                break
+
             # Step 1
             D = self.Bop._matmat(Xj) + Z / sigma
             upper = self.weights
@@ -233,124 +303,84 @@ class SSNAL:
 
             alpha = self.weights[zet_less_one] / (sigma * norms_D_hat)
 
-            # nz_norms_D = norms_D.nonzero()
-            # alpha = np.empty(self.E)
-            # alpha.fill(np.inf)
-            # alpha[nz_norms_D] = (
-            #     sigmainv * self.weights[nz_norms_D] / norms_D[nz_norms_D]
-            # )
-            # alpha_less_1 = alpha < 1
-            # alpha_less_1_explicit = np.arange(self.E)[alpha_less_1]
-            # hat_r = self.nz_r[alpha_less_1]
-            # hat_c = self.nz_c[alpha_less_1]
-            # hat_alpha = alpha[alpha_less_1]
-
-            # # M = np.zeros((self.n, self.n))
-            # # M[hat_r, hat_c] = 1 - hat_alpha
-            # # M[hat_c, hat_r] = 1 - hat_alpha
-
-            # Mij = np.zeros(self.E)
-            # Mij[alpha_less_1] = 1 - hat_alpha
-
-            # indices = np.arange(self.E)[alpha_less_1]
-            # cM = scipy.sparse.lil_array((self.n, self.E))
-            # cM[hat_r, indices] = 1 - hat_alpha
-            # # cM = cM.tocsr()
-            # # cM_bar = scipy.sparse.lil_array((self.n, self.E))
-            # cM[hat_c, indices] = -(1 - hat_alpha)
-            # cM = cM.tocsr()
-            # cM_map = cM - cM_bar
-
             def V(X):
-                # Y = X @ (cM)
-                # Y = Mij * self.Bop._matmat(X)
-                # BadjY = self.Bop._adjoint(Y)
-                # aux_X = X[:, hat_r] - X[:, hat_c]
-                # aux_D = D[:, alpha_less_1]
-                # products = np.sum((X[:, hat_r] - X[:, hat_c]) * D[:, alpha_less_1], axis=0)
-                # products = sum_with_numba(X, hat_r, hat_c, D, alpha_less_1_explicit)
-                # rho = np.zeros(self.E)
-                # rho[alpha_less_1] = hat_alpha / (norms_D[alpha_less_1] ** 2) * products
-                # W = rho * D
-                # BadjW = self.Bop._adjoint(W)
-
                 map_X = self.Bop._matmat(X)
                 map_X_hat = map_X[:, zet_less_one]
                 rho = np.einsum("j, ij,ij->j", alpha, map_X_hat, D_sel)
                 map_X[:, zet_less_one] = map_X_hat * alpha - D_sel * rho
                 return X + sigma * self.Bop._adjoint(map_X)
 
-            grad_phi_Xj = self.grad_phi(Xj, Z, sigma)
-            cg_tolerance = fnorm(grad_phi_Xj) ** (1 + tau)
+
+            cg_tolerance = norm_grad_phi_Xj ** (1 + tau)
+            #print('cg tolerance: ', cg_tolerance)
             dj = np.zeros_like(Xj)
-            residual = -grad_phi_Xj
-            sd = residual
+            residual = grad_phi_Xj
+            sd = -residual
             beta = 0
             for j in range(cg_max_iter):
                 # print('cg iter: ', j)
                 if j != 0:
-                    beta = np.sum(np.square(residual)) / ssres
-                    sd = residual + beta * sd
+                    beta = np.sum(np.square(residual)) / ssres #new over old
+                    sd = -residual + beta * sd
                 Vsd = V(sd)
                 sdVsd = np.sum(sd * Vsd)
                 ssres = np.sum(np.square(residual))
                 dj = dj + ssres / sdVsd * sd
-                residual = residual - ssres / sdVsd * Vsd
-
+                residual = residual + ssres / sdVsd * Vsd
                 stopping = fnorm(grad_phi_Xj + V(dj))
-                # print('stopping: ', stopping)
+                #print('stopping: ', stopping)
                 if stopping <= min(eta, cg_tolerance):
                     break
+
             # Step 2
             # print('step 2')
-            alpj = 1
 
-            phiXj = self.phi(Xj, Z, sigma)
-            innergpXjdj = matinner(grad_phi_Xj, dj)
-            # mj = 0
-            # while (
-            #         self.phi(Xj + delta**mj * dj, Z, sigma)
-            #         > phiXj + mu * delta**mj * innergpXjdj
-            #     ):
-            #     mj += 1
-            mj = 0
-            alpj = 1
-            while self.phi(Xj + alpj * dj, Z, sigma) > phiXj + mu * alpj * innergpXjdj:
-                alpj *= delta
-            # if (self.phi(Xj + delta**mj * dj, Z, sigma)
-            #     > phiXj + mu * delta**mj * innergpXjdj):
-            #     while (
-            #         self.phi(Xj + delta**mj * dj, Z, sigma)
-            #         > phiXj + mu * delta**mj * innergpXjdj
-            #     ):
-            #         mj += 1
-            # else:
-            #     while (
-            #         self.phi(Xj + delta**mj * dj, Z, sigma)
-            #         <= phiXj + mu * delta**mj * innergpXjdj
-            #     ):
-            #         mj -= 1
-            #         if mj == -1:
-            #             break
-            #     mj += 1
-            # print('mj: ', mj)
-            # mjs[iter] = mj
-            # print(score)
-            # if iter > 0:
-            #     if mjs[iter] == mjs[iter - 1]:
-            #         score += 1
-            #         if score > 9:
-            #             break
-            #     else:
-            #         score = 0
-            # alpj = delta**mj
+            alpj = self.line_search(Xj, dj, Z, sigma, mu)
+            # while self.phi(Xj + alpj * dj, Z, sigma) > phiXj + mu * alpj * innergpXjdj:
+            #     alpj *= delta
 
             # Step 3
-            Xj_prev = Xj
             Xj = Xj + alpj * dj
+        
             if alpj < 1e-10:
                 break
-            if fnorm(self.grad_phi(Xj, Z, sigma)) <= tolerance:
-                break
 
+
+        #print(itera, Xj)
         return Xj
+
+    def line_search(self, Xj, dj, Z, sigma, mu=1e-4, c2 = 0.9, alp_max = 20):
+        alp_prev = 0
+        alpi = 0.5
+        f = lambda alp: self.phi(Xj + alp * dj, Z, sigma)
+        f0 = f(0)
+        f_1 = lambda alp: matinner(self.grad_phi(Xj + alp * dj, Z, sigma), dj)
+        f_1_0 = f_1(0)
+        max_iter = 10
+        for i in range(max_iter):
+            curr = f(alpi)
+            if curr > f0 + mu * alpi * f_1_0:
+                alpi = self.zoom(alp_prev, alpi, mu, c2, f, f_1, f0, f_1_0)
+                return alpi
+            grad = f_1(alpi)
+            if abs(grad) <= -c2 * f_1_0:
+                return alpi
+            if grad >= 0:
+                alpi = self.zoom(alpi, alp_prev, mu, c2, f, f_1, f0, f_1_0)
+                return alpi
+            alp_prev = alpi
+            alpi = min(2 * alpi, alp_max)
+        return alpi
+
+    def zoom(self, alp_low, alp_high, mu, c2, f, f_1, f0, f_1_0):
+        while True:
+            alp_j = (alp_low + alp_high) / 2
+            if f(alp_j) > f0 + mu * alp_j * f_1_0:
+                alp_high = alp_j
+            else:
+                grad = f_1(alp_j)
+                if abs(grad) <= -c2 * f_1_0:
+                    return alp_j
+                if grad * (alp_high - alp_low) >= 0:
+                    alp_high = alp_low
+                alp_low = alp_j
